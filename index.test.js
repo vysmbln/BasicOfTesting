18230d16530f2d36bae8cd31a4fdc29a45a31971
vysmbln <vysmbln@users.noreply.github.com>
2020-06-10 21:47:17 -0400

    initial data

---
+++ index.test.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+
+const { forEach, map } = require('./index');
+
+const test = (desc, fn) => {
+    console.log('----', desc);
+    try {
+        fn();
+    } catch (err) {
+        console.log(err.message)
+    }
+};
+
+test('The forEach function', () => {
+    let sum = 0;
+    forEach([1, 2, 3], value => {
+        sum += value;
+    });
+
+    assert.strictEqual(sum, 6, 'Expected forEach to sum the array')
+
+})
+
+test('The map function', () => {
+    const result = map ([1, 2 ,3], value => {
+        return value * 2;
+    });
+
+    assert.deepStrictEqual(result, [2, 4, 6])
+
+})
+
+
+
